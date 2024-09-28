ee42d359dbd5e74ecdcdb4ad32add97ce9697768
niinapiina <niina.pietila@outlook.com>
2024-09-28 19:10:59 +0300

    Versio 3
    Lisätty if else

--- Main.java
+++ Main.java
@@ -9,6 +9,13 @@ public class Main {
         number2= 10;
         number3= 10;
 
+        if (number1 == number2 && number2 == number3){
+            System.out.println("Numbers are equal.");
+           }
+           else {
+            System.out.println("Numbers are not equal.");
+           }
+
         
     }
 }
